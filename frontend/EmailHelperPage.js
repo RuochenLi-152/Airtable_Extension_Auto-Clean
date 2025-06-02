0e2ec982ca86f258d80ee79b3eef84575578c502
RuochenLi-152 <rl3292@columbia.edu>
2025-06-02 12:38:48 -0400

    save the idea for now

---
+++ frontend/EmailHelperPage.js
@@ -0,0 +1,5 @@
+/* Add health form email address generator
+
+2 options - all parents emails or health form needs update emails
+Then prompt to choose field
+Then generate the string and add a copy button */
